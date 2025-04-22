764b6c0569afe4a7e28b40ec117f81d8e5e23657
IgnI <54883747+IgnIVertiKalCaD@users.noreply.github.com>
2025-04-22 04:26:09 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-mkdir /etc/xray
+mkdir /etc/xray/
 cp config.json /etc/xray
 
-mkdir /etc/nftables.d
+mkdir /etc/nftables.d/
 cp proxy.conf /etc/nftables.d
+
+cp xray-proxy.nix /etc/nixos/
 
 echo "cool. Run nixos-rebuild test"
